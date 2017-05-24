1011c263e224d03f656a18ed6512df088951176c
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-23 19:25:57 -0700

    Initialized Webpack template

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const WDS_PORT = 8000;
+
+module.exports = {
+  entry: [
+    './client',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+    publicPath: 'dist'
+  },
+  watch: true,
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [ path.resolve(__dirname, './client') ],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /(\.css|\.scss)$/,
+        include: [ path.resolve(__dirname, './client/css') ],
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: './client',
+    inline: true,
+    hot: true,
+  },
+}
