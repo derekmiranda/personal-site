c467c4dea6b800d9d08a0a221e7071ed6f4872f8
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-27 10:29:05 -0700

    Import velocity

--- client/index.js
+++ client/index.js
@@ -1,10 +1,2 @@
 import globalStyle from './css/global.scss';
-
-// import React from 'react';
-// import { render } from 'react-dom';
-// import App from './components/app.jsx';
-
-// render(
-//   <App />,
-//   document.getElementById('main')
-// );
+import $ from 'velocity-animate';
