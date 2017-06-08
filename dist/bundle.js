034abaa7b38d02d3c9c0009c218f849ee33a3fbd
Derek Miranda <derek.miranda.1992@gmail.com>
2017-06-08 16:44:42 -0700

    Added responsive pixel profile image

--- dist/bundle.js
+++ dist/bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "80598d50070cb0911bc5"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6d6e77b6edb7def8e0dc"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
