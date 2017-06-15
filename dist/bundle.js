8009559c5bc15ddcc43bdffe85a9e021acf4fbbd
Derek Miranda <derek.miranda.1992@gmail.com>
2017-06-14 17:34:14 -0700

    Used font-size to resize text among diff screen sizes

--- dist/bundle.js
+++ dist/bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6d6e77b6edb7def8e0dc"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "77febd42b531353447da"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
