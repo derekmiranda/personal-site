48b4f872745e32449f92d1a66ad8f69f58912c07
Derek Miranda <derek.miranda.1992@gmail.com>
2017-09-09 19:15:05 -0700

    Split into funcs

--- client/index.js
+++ client/index.js
@@ -1,7 +1,8 @@
-import style from './css/global.scss';
 import Velocity from 'velocity-animate';
 
-const animOrder = [
+import style from './css/global.scss';
+
+const ANIM_ORDER = [
   'pixelme',
   'header',
   'description',
@@ -12,16 +13,20 @@ const FADE_IN_STEP = 500;
 
 function main() {
   if (document.readyState === 'interactive') {
-
-    animOrder.forEach((item, i) => {
-      const elem = document.getElementById(item);
-
-      Velocity(elem, 'fadeIn', {
-        duration: BASE_FADE_IN_TIME + i * FADE_IN_STEP,
-      });
-    });
-
+    animateInElements();
   }
+}
+
+function animateInElements() {
+  ANIM_ORDER.forEach(animateInElement);
+}
+
+function animateInElement(item, i) {
+  const elem = document.getElementById(item);
+
+  Velocity(elem, 'fadeIn', {
+    duration: BASE_FADE_IN_TIME + i * FADE_IN_STEP,
+  });
 }
 
 document.addEventListener('readystatechange', main);
