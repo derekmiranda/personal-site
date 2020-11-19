93be53016f0ccfc71f5f574ae78f12b8213141e8
Derek Miranda <derek.miranda.1992@gmail.com>
2020-11-19 15:22:53 -0800

    Adjusts shaders page layout

--- src/shaders/shaders.js
+++ src/shaders/shaders.js
@@ -4,17 +4,50 @@ import circleShader from "./circle.glsl";
 import worleyShader from "./worley.glsl";
 import glitchShader from "./glitch.glsl";
 
-const canvasEls = document.getElementsByClassName("glslCanvas");
+const canvasContainers = document.getElementsByClassName("canvas-container");
 const shaders = {
   water: waterShader,
   circle: circleShader,
   worley: worleyShader,
   glitch: glitchShader,
 };
+const canvases = [];
+const imgs = [];
 
-for (let i = 0; i < canvasEls.length; i++) {
-  const canvas = canvasEls[i];
+for (let i = 0; i < canvasContainers.length; i++) {
+  const canvas = canvasContainers[i].querySelector("canvas");
+  const img = canvasContainers[i].querySelector("img");
+
+  canvases.push(canvas);
+  imgs.push(img);
+
   const shader = canvas.dataset.shaderName;
-  const sandbox = new GlslCanvas(canvasEls[i]);
+  const sandbox = new GlslCanvas(canvas);
   sandbox.load(shaders[shader]);
+  sandbox.paused = true;
+
+  function toggleShader() {
+    if (img.classList.contains("img-hide")) {
+      img.classList.remove("img-hide");
+    } else {
+      img.classList.add("img-hide");
+    }
+    sandbox.paused = !sandbox.paused;
+  }
+
+  const toggleHandler = throttle(toggleShader, 200);
+
+  canvasContainers[i].addEventListener("click", toggleHandler);
+  canvasContainers[i].addEventListener("touchstart", toggleHandler);
+}
+
+// util(s)
+function throttle(fn, time) {
+  let lastTime;
+  return function (...args) {
+    if (!lastTime || Date.now() > lastTime + time) {
+      fn(...args);
+      lastTime = Date.now();
+    }
+  };
 }
